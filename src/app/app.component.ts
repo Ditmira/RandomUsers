1b052ce2588509e2465ac6fb7e28a5b112f755df
DIT\ditmira <ditmiragjini@gmail.com>
2022-08-27 16:51:45 +0200

    fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RandomUsers';
+  title: string;
+
+  constructor() {
+    this.title = 'Random Users';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {UserInfoComponent} from "./user-info/user-info.component";
+import {Location} from "./modules/location/location";
+import {User} from "./modules/user/user";
+import {Name} from "./modules/name/name";
+import {AppRoutingModule} from "./app-routing.module";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserInfoComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
